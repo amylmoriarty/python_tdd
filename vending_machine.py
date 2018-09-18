06342f0e29aa08b69a08ea6aadaa7b58d633070a
Amy Moriarty <lostjrt16@gmail.com>
2018-09-18 17:13:49 +0000

    vending machine code refactored - all tests passing

--- vending_machine.py
+++ vending_machine.py
@@ -1,13 +1,16 @@
 from byotest import *
 
-def get_change(amount):
-    if amount == 0:
-        return []
-    
-    if amount in [100, 50, 20,10, 5, 2, 1]:
-        return [amount]
-        
-    return [2,1]
+usd_coins = [100, 50, 25, 10, 5, 1]
+eur_coins = [100, 50, 20, 10, 5, 2, 1]
+
+def get_change(amount, coins = eur_coins):
+   
+    change = []
+    for coin in coins:
+        while coin <= amount:
+            amount -= coin
+            change.append(coin)
+    return change
 
 test_are_equal (get_change (0), [])
 test_are_equal (get_change (1), [1])
@@ -18,5 +21,8 @@ test_are_equal (get_change (20), [20])
 test_are_equal (get_change (50), [50])
 test_are_equal (get_change (100), [100])
 test_are_equal (get_change (3), [2,1])
+test_are_equal (get_change (7), [5,2])
+test_are_equal (get_change (9), [5, 2,2])
+test_are_equal (get_change (35, usd_coins), [25, 10])
 
 print ("All tests pass!")
